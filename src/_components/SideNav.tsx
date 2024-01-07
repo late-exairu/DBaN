fc97cc59ea45396d7000c9b5e549ebdb07782f0a
Oleg Shelest <lateexairu@gmail.com>
2024-01-07 03:38:54 +0200

    Update SideNav styling

--- src/_components/SideNav.tsx
+++ src/_components/SideNav.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function SideNav() {
   return (
     <aside className="relative z-10 hidden min-w-44 lg:block">
-      <ul className="my-5 flex flex-col gap-2 text-2xl font-black">
+      <ul className="sticky top-[84px] my-5 flex flex-col gap-2 text-2xl font-black">
         <Link href="/">Home</Link>
       </ul>
     </aside>
