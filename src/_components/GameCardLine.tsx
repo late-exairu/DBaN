4b0e79388b5f4a5c14cc6a21ba8b7cfa25b9cd48
Oleg Shelest <lateexairu@gmail.com>
2024-01-08 22:22:01 +0200

    Refactor GameCardLine and MetacriticScore components

--- src/_components/GameCardLine.tsx
+++ src/_components/GameCardLine.tsx
@@ -28,10 +28,10 @@ export default function GameCard(props: GameData) {
   return (
     <Link
       href={`/game/${id}`}
-      className="group relative flex overflow-clip rounded-lg bg-slate-950 px-5 py-4 text-sm leading-6 shadow-md transition-[border-radius]"
+      className="group relative flex overflow-clip rounded-lg bg-slate-950 px-4 py-3 text-sm leading-6 shadow-md transition-[border-radius] sm:px-5 sm:py-4"
     >
       <Image
-        className="absolute inset-x-0 inset-y-0 scale-[101%] object-cover object-center brightness-75 duration-300 ease-in-out will-change-transform group-hover:scale-110 group-hover:brightness-90 sm:top-[-100px]"
+        className="absolute inset-x-0 inset-y-0 scale-[101%] object-cover object-center duration-300 ease-in-out will-change-transform group-hover:scale-110 sm:top-[-100px]"
         width={1100}
         height={400}
         sizes="(max-width: 768px) 100vw, (max-width: 1280px) 50vw, 33vw"
@@ -40,14 +40,19 @@ export default function GameCard(props: GameData) {
         alt={name}
       />
 
-      <div className="absolute inset-0 bg-gradient-to-r from-black" />
+      <div className="absolute inset-0 bg-gradient-to-r from-black/70 to-black/20" />
 
-      <MetacriticScore score={metacritic} />
+      <MetacriticScore className="absolute right-4 top-4" score={metacritic} />
 
       <div className="relative w-full text-white">
-        <Platforms className="mb-1" platforms={platforms} />
+        <Platforms
+          className="absolute bottom-1 right-0"
+          platforms={platforms}
+        />
 
-        <h3 className="text-lg font-bold sm:text-xl">{name}</h3>
+        <h3 className="pr-14 text-lg font-bold leading-tight sm:text-xl md:pt-0">
+          {name}
+        </h3>
 
         <div className="relative text-xs">
           <div className="flex flex-col gap-1 py-0.5 sm:flex-row">
--- src/_components/ui/metacriticScore.tsx
+++ src/_components/ui/metacriticScore.tsx
@@ -1,7 +1,9 @@
 import logoMetacritic from "@/public/logo-metacritic.svg";
+import { twMerge } from "tailwind-merge";
 import Image from "next/image";
 
 type MetacriticScoreProps = {
+  className?: string;
   score: number;
 };
 
@@ -11,9 +13,17 @@ type StaticImageData = {
   width: number;
 };
 
-export default function MetacriticScore({ score }: MetacriticScoreProps) {
+export default function MetacriticScore({
+  score,
+  className,
+}: MetacriticScoreProps) {
   return (
-    <div className="absolute right-2 top-2 flex w-12 items-center gap-1 rounded-sm bg-slate-100 px-1 text-center font-bold text-slate-900">
+    <div
+      className={twMerge(
+        `absolute right-2 top-2 flex w-12 items-center gap-1 rounded-sm bg-slate-100 px-1 text-center font-bold text-slate-900`,
+        className,
+      )}
+    >
       <Image
         className="h-4 w-4"
         src={logoMetacritic as StaticImageData}
