c1eb0746c0a70c0b9ec3e35692bee6f4f7458cc1
Oleg Shelest <lateexairu@gmail.com>
2024-01-14 00:51:01 +0200

    Fix type in getGameScreenshots function

--- src/utils/getGameScreenshots.ts
+++ src/utils/getGameScreenshots.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
-import { type GameScreenshots } from "@/types";
+import { type GameScreenshotsRes } from "@/types";
 
 const API_KEY = process.env.NEXT_PUBLIC_RAWG_API_KEY;
 
-const getGameScreenshots = (id: number): Promise<GameScreenshots> => {
+const getGameScreenshots = (id: number): Promise<GameScreenshotsRes> => {
   return axios
     .get(`https://api.rawg.io/api/games/${id}/screenshots?key=${API_KEY}`)
     .then((res) => {
-      return res.data as GameScreenshots;
+      return res.data as GameScreenshotsRes;
     });
 };
 
