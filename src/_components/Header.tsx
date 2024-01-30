3cc0562e806a41181397d62de11da690b65945e9
Oleg Shelest <lateexairu@gmail.com>
2024-01-30 17:18:52 +0200

    Update Header gaps

--- src/_components/Header.tsx
+++ src/_components/Header.tsx
@@ -6,7 +6,7 @@ import Burger from "@/components/Burger";
 export default function Header() {
   return (
     <header className="fixed inset-x-0 top-0 z-20 bg-background shadow-sm">
-      <div className="container flex gap-5 py-2">
+      <div className="container flex gap-2 py-2 md:gap-5">
         <Logo />
         <Input type="text" placeholder="Search" />
         <Button variant="outline">Button</Button>
