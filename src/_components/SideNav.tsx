ea6c0cd17216dc6d0adc5a5fe3d9d4ac330d5808
Oleg Shelest <lateexairu@gmail.com>
2024-01-30 17:01:36 +0200

    Update SideNav component styling

--- src/_components/SideNav.tsx
+++ src/_components/SideNav.tsx
@@ -13,7 +13,7 @@ export default function SideNav(props: SideNavProps) {
 
   return (
     <aside
-      className={`${className} ${isMenuOpen ? "translate-x-0" : "translate-x-full"} fixed inset-0 left-auto z-10 min-h-svh w-full min-w-44 bg-white px-5 shadow transition-transform sm:w-auto lg:relative lg:block lg:translate-x-0 lg:px-0 lg:shadow-none `}
+      className={`${className} ${isMenuOpen ? "translate-x-0" : "translate-x-full"} fixed inset-0 left-auto z-10 min-h-svh w-full min-w-44 bg-white px-5 shadow transition-transform sm:w-auto lg:relative lg:block lg:translate-x-0 lg:bg-transparent lg:px-0 lg:shadow-none `}
     >
       <ul className="sticky top-[84px] my-5 flex flex-col gap-2 text-lg font-black lg:text-2xl">
         <Link href="/">Home</Link>
