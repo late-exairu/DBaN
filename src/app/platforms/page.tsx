7a6121eb6280c2f8e735e2daf7cb679681653f06
Oleg Shelest <lateexairu@gmail.com>
2024-02-11 10:21:55 +0200

    Add headings to genres and platforms pages

--- src/app/platforms/page.tsx
+++ src/app/platforms/page.tsx
@@ -29,6 +29,10 @@ export default async function page() {
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <main className="relative flex w-full flex-col">
+        <h3 className="my-3 text-2xl font-black md:my-4 md:text-3xl xl:my-5 xl:text-4xl">
+          Platforms
+        </h3>
+
         <Platforms />
       </main>
     </HydrationBoundary>
