75c6adeb3a9539caedbb83b9b90f908dcb79bc03
Oleg Shelest <lateexairu@gmail.com>
2024-02-26 13:51:10 +0200

    Add BrowsePage component

--- src/_components/PageBgImage.tsx
+++ src/_components/PageBgImage.tsx
@@ -10,9 +10,9 @@ export default function PageBgImage(props: PageBgProps) {
   const { background } = props;
 
   return (
-    <div className="absolute inset-0 z-0 opacity-10">
+    <div className="pointer-events-none absolute inset-x-0 top-0 z-0 h-dvh opacity-10">
       <Image
-        className="inset-0 object-cover grayscale"
+        className="absolute inset-0 object-cover grayscale"
         src={background ?? "/game-image-placeholder.png"}
         fill={true}
         alt={background ? "Background image" : "Placeholder image"}
--- src/app/genres/[id]/page.tsx
+++ src/app/genres/[id]/page.tsx
@@ -1,90 +1,56 @@
-"use client";
-
-import { useState, Suspense } from "react";
-import { useQuery, keepPreviousData } from "@tanstack/react-query";
-import { useSearchParams } from "next/navigation";
-import Cards from "@/components/Cards";
-import Pager from "@/components/Pager";
-import PageBgImage from "@/components/PageBgImage";
-import { getGames, getGenreDetails } from "@/utils/apiUtils";
-import { type ApiResponse, type GameData, type Platform } from "@/types";
+import { Suspense } from "react";
+import { type Metadata } from "next";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { getGenreDetails } from "@/utils/apiUtils";
+import BrowsePage from "@/components/BrowsePage";
 
 type Props = {
-  genres: string;
+  params: { id: string; name: string };
 };
 
-function PageContent(props: Props) {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const data = await getGenreDetails(params.id);
+
+  return {
+    title: `DBaN - ${data.name}`,
+  };
+}
+
+export async function PageContent(props: { genres: string }) {
   const { genres } = props;
-  const searchParams = useSearchParams();
-  const [sortBy, setSortBy] = useState("-metacritic");
-  const [page, setPage] = useState(
-    Number.parseInt(searchParams.get("page") ?? "1"),
-  );
 
-  function handlePageChange(page: number) {
-    setPage(page);
-  }
-
-  function handleSortByChange(sortBy: string) {
-    setSortBy(sortBy);
-  }
-
-  const { data, isLoading, error } = useQuery<ApiResponse<GameData>>({
-    queryKey: ["games", page, sortBy, genres],
-    queryFn: () => getGames(page, sortBy, undefined, genres),
-    placeholderData: keepPreviousData,
-    staleTime: 600000, // 10 minutes
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 600000, // 10 minutes
+      },
+    },
   });
 
-  const genreDetails = useQuery<Platform>({
+  // await queryClient.prefetchQuery({
+  //   queryKey: ["games", page, sortBy, genres],
+  //   queryFn: () => getGames(page, sortBy, undefined, genres),
+  // });
+
+  await queryClient.prefetchQuery({
     queryKey: ["genreDetails", genres],
     queryFn: () => getGenreDetails(genres),
-    placeholderData: keepPreviousData,
-    staleTime: 600000, // 10 minutes
   });
 
-  genreDetails.error ? `Error: ${genreDetails?.error?.message}` : null;
-  genreDetails.isLoading ? "Loading..." : null;
-
   return (
-    <>
-      <PageBgImage background={genreDetails?.data?.image_background} />
-      <main className="flex flex-1 flex-col">
-        <h3 className="my-3 text-2xl font-black md:my-4 md:text-3xl xl:my-5 xl:text-4xl">
-          {genreDetails?.data?.name} Games
-        </h3>
-
-        {genreDetails?.data?.description && (
-          <div
-            className="mb-3 text-sm md:mb-4"
-            dangerouslySetInnerHTML={{
-              __html: genreDetails?.data?.description,
-            }}
-          />
-        )}
-
-        <Cards
-          data={data}
-          isLoading={isLoading}
-          error={error}
-          sortBy={sortBy}
-          handleSortByChange={handleSortByChange}
-        />
-
-        {data?.count && (
-          <Pager
-            itemsCount={data.count}
-            currentPage={page}
-            handlePageChange={handlePageChange}
-          />
-        )}
-      </main>
-    </>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <BrowsePage genres={genres} />
+    </HydrationBoundary>
   );
 }
 
-export default function Page({ params }: { params: { id: number } }) {
+export default async function Page({ params }: { params: { id: number } }) {
   const { id } = params;
+  console.log("id", id);
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
