95b44260d189b39ad7615d32641667c179573f6b
Oleg Shelest <lateexairu@gmail.com>
2024-02-29 11:22:05 +0200

    Fix build errors

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,8 +3,8 @@ import postgres from "postgres";
 import * as schema from "./schema";
 
 const connectionString =
-  process.env.DATABASE_URL ||
-  "postgres://postgres:postgres@localhost:5432/postgres";
+  process.env.DATABASE_URL ??
+  "postgres://postgres.dizkqkzahdkunwpjlyst:5QWW33VUfhIar1sP@aws-0-us-west-1.pooler.supabase.com:5432/postgres";
 
 const client = postgres(connectionString);
 export const db = drizzle(client, { schema });
