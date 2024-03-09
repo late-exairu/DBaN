dc0f93630b89d63a1713bceab329c3d685a0b013
Oleg Shelest <lateexairu@gmail.com>
2024-03-09 17:04:15 +0200

    Refactor user profile page

---
+++ src/_components/Profile.tsx
@@ -0,0 +1,92 @@
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
+import { eq } from "drizzle-orm";
+import { users } from "@/db/schema";
+import { db } from "@/db";
+import { auth } from "@/auth";
+
+type ProfileProps = {
+  user: {
+    id: string;
+    name: string;
+    email: string;
+    bio: string;
+    image: string;
+  };
+  personal?: boolean;
+};
+
+export default async function Profile(props: ProfileProps) {
+  const { user, personal } = props;
+  const session = await auth();
+
+  async function handleSave(formData: FormData) {
+    "use server";
+
+    await db
+      .update(users)
+      .set({
+        name: formData.get("name") as string,
+        bio: formData.get("bio") as string,
+      })
+      .where(eq(users.id, session?.user?.id ?? ""));
+
+    console.log("Save");
+  }
+
+  return (
+    <main className="relative flex flex-col">
+      <h1 className="my-3 text-2xl font-black md:my-4 md:text-3xl xl:my-5 xl:text-4xl">
+        Profile
+      </h1>
+      <Tabs defaultValue="account" className="max-w-[600px]">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+
+        <TabsContent className="mt-6" value="account">
+          <form action={handleSave} className="flex flex-col gap-4">
+            <div className="col-span-2">
+              <Image
+                src={user?.image ?? "/game-image-placeholder.png"}
+                alt={user?.name ?? "User Image Placeholder"}
+                width={96}
+                height={96}
+                className="block rounded-md"
+              />
+            </div>
+
+            <div className="col-span-2 flex gap-4">
+              <div>
+                <label className="text-sm">Name</label>
+                <Input
+                  type="text"
+                  name="name"
+                  defaultValue={user?.name ?? ""}
+                />
+              </div>
+              <div>
+                <label className="text-sm">Email</label>
+                <Input type="text" placeholder={user?.email} readOnly />
+              </div>
+            </div>
+            <div className="col-span-2">
+              <label className="text-sm">Bio</label>
+              <Textarea name="bio" defaultValue={user?.bio ?? ""} />
+            </div>
+
+            <Button className="mt-4" type="submit">
+              Save
+            </Button>
+          </form>
+        </TabsContent>
+
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
+    </main>
+  );
+}
--- src/app/user/[slug]/page.tsx
+++ src/app/user/[slug]/page.tsx
@@ -1,90 +1,37 @@
 import { eq } from "drizzle-orm";
-import Image from "next/image";
 import { auth } from "@/auth";
 import { users } from "@/db/schema";
 import { db } from "@/db";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Button } from "@/components/ui/button";
+import Profile from "@/components/Profile";
 
-type Props = {
-  slug: string;
-};
+export default async function Page({ params }: { params: { slug: string } }) {
+  const { slug } = params;
 
-export default async function Page(props: Props) {
-  const { slug } = props;
   const session = await auth();
-  console.log(slug);
 
   const result = await db.query.users.findFirst({
     where: eq(users.id, session?.user?.id ?? ""),
   });
 
-  async function handleSave(formData: FormData) {
-    "use server";
-
-    await db
-      .update(users)
-      .set({
-        name: formData.get("name") as string,
-        bio: formData.get("bio") as string,
-      })
-      .where(eq(users.id, session?.user?.id ?? ""));
-
-    console.log("Save");
+  if (!session) {
+    return <div>Unauthorized</div>;
   }
 
-  return (
-    <main className="relative flex flex-col">
-      <h1 className="my-3 text-2xl font-black md:my-4 md:text-3xl xl:my-5 xl:text-4xl">
-        Profile
-      </h1>
-      <Tabs defaultValue="account" className="max-w-[600px]">
-        <TabsList>
-          <TabsTrigger value="account">Account</TabsTrigger>
-          <TabsTrigger value="password">Password</TabsTrigger>
-        </TabsList>
+  if (!result) {
+    return <div>User not found</div>;
+  }
 
-        <TabsContent className="mt-6" value="account">
-          <form action={handleSave} className="flex flex-col gap-4">
-            <div className="col-span-2">
-              <Image
-                src={result?.image ?? "/game-image-placeholder.png"}
-                alt={result?.name ?? "User Image Placeholder"}
-                width={96}
-                height={96}
-                className="block rounded-md"
-              />
-            </div>
+  if (slug !== session?.user?.name) {
+    return <div>Other user profile</div>;
+  }
 
-            <div className="col-span-2 flex gap-4">
-              <div>
-                <label className="text-sm">Name</label>
-                <Input
-                  type="text"
-                  name="name"
-                  defaultValue={result?.name ?? ""}
-                />
-              </div>
-              <div>
-                <label className="text-sm">Email</label>
-                <Input type="text" placeholder={result?.email} readOnly />
-              </div>
-            </div>
-            <div className="col-span-2">
-              <label className="text-sm">Bio</label>
-              <Textarea name="bio" defaultValue={result?.bio ?? ""} />
-            </div>
+  const user = {
+    id: result.id,
+    name: result.name ?? "",
+    email: result.email,
+    bio: result.bio ?? "",
+    image: result.image ?? "",
+  };
 
-            <Button className="mt-4" type="submit">
-              Save
-            </Button>
-          </form>
-        </TabsContent>
-
-        <TabsContent value="password">Change your password here.</TabsContent>
-      </Tabs>
-    </main>
-  );
+  return <Profile user={user} personal />;
 }
